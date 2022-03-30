8e726bf990a86a7fdaa8e7231fca857dd47661b6
Klaudia <kosaa96@gmail.com>
2022-03-30 21:17:53 +0200

    feat: emit message from client to server

--- consts.ts
+++
@@ -1,3 +0,0 @@
-export const OUT_MESSAGE = "outMessage";
-export const IN_MESSAGE = "inMessage";
-export const OLD_MESSAGES = "oldMessages";
--- socket/server.ts
+++ socket/server.ts
@@ -1,13 +1,13 @@
 import { Server } from "socket.io";
-import { IN_MESSAGE, OLD_MESSAGES, OUT_MESSAGE } from "../consts";
 
 export function setupHandlers(io: Server) {
   io.on("connection", (socket) => {
     console.log("Client connected");
     socket.emit("connected", "Connected");
+
     socket.on("message", (arg) => {
       console.log(arg);
-      socket.broadcast.emit("connected", arg);
+      socket.broadcast.emit("message", arg);
     });
   });
 }
